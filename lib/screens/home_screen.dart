20a42e6a9e5d73abb473acaebd98d4eac6954696
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2020-10-25 01:48:36 +0530

    Adding all the methods for longing and signing up user (#5)
    * auth file added
    
    * Adding all the methods for loging and sgining up user

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import '../widgets/color.dart';
 import '../widgets/home_screen_widgets/h_s_app_bar_title.dart';
 import '../widgets/home_screen_widgets/h_s_profile_image.dart';
 import '../widgets/home_screen_widgets/h_s_list_view.dart';
+import '../widgets/home_screen_widgets/h_s_test_button.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -14,6 +15,8 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: appBarColor,
         title: HSAppBarTitle(),
         actions: [
+          /// This button is for debuging purpose
+          HSTestButton(),
           HSProfileImage(),
         ],
       ),
---
+++ lib/widgets/home_screen_widgets/h_s_test_button.dart
@@ -0,0 +1,36 @@
+import 'package:engrave/provider/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HSTestButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Icons.one_k,
+        color: Colors.black,
+      ),
+      onPressed: () async {
+        print("OK");
+        // await Provider.of<Auth>(context, listen: false).signUP(
+        //   email: "testgmial.com",
+        //   password: "test123",
+        //   username: "test123",
+        // );
+
+        // if (await Provider.of<Auth>(context, listen: false).tryAutoLogin()) {
+        //   print("Loged in");
+        // } else {
+        //   await Provider.of<Auth>(context, listen: false).logIn(
+        //     email: "testgmial.com",
+        //     password: "test123",
+        //   );
+        // }
+
+        print(Provider.of<Auth>(context, listen: false).isAuthenticated);
+
+        //await Provider.of<Auth>(context, listen: false).logOut();
+      },
+    );
+  }
+}
