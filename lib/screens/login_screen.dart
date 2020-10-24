bb77ec259d6bff20d8d4522774412e0a3453a0c7
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2020-10-25 03:15:48 +0530

    Adding patch for unable to merge request (#8)
    * auth file added
    
    * Adding all the methods for loging and sgining up user
    
    * user provider & model added
    
    * Adding patch for unable to merge request

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,70 +1,91 @@
+import 'package:engrave/provider/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import './home_screen.dart';
-// import 'package:email_validator/email_validator.dart';
-// import '../main_properties/theme.dart';
-// import '../main_properties/routs.dart';
+import 'package:provider/provider.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static const routeName = 'login-screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  var _isLoading = false;
   final userController = TextEditingController();
+
   final passController = TextEditingController();
+
+  void onSaved(BuildContext context) {
+    if (userController.text.isNotEmpty && passController.text.isNotEmpty) {
+      Provider.of<Auth>(context, listen: false).logIn(
+        email: userController.text,
+        password: passController.text,
+      );
+      print(userController.text);
+    } else {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Login Screen"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                child: SvgPicture.asset(
-                  'assets/images/4.svg',
-                  fit: BoxFit.cover,
-                ),
+      appBar: AppBar(
+        title: Text("Login Screen"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              child: SvgPicture.asset(
+                'assets/images/4.svg',
+                fit: BoxFit.cover,
               ),
-              Container(
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Card(
                 margin: EdgeInsets.all(10),
-                child: Card(
-                  margin: EdgeInsets.all(10),
-                  elevation: 4,
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: <Widget>[
-                        TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Username',
-                            prefixIcon: Icon(Icons.account_circle),
-                          ),
-                          controller: userController,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                elevation: 4,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          prefixIcon: Icon(Icons.account_circle),
                         ),
-                        TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            prefixIcon: Icon(Icons.lock),
-                          ),
-                          controller: passController,
+                        controller: userController,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          prefixIcon: Icon(Icons.lock),
                         ),
-                        RaisedButton(
-                          onPressed: () {
-                            Navigator.of(context)
-                                .pushNamed(HomeScreen.routeName);
-                          },
-                          child: Text('Login'),
-                          textColor: Colors.purple,
-                        ),
-                      ],
-                    ),
+                        controller: passController,
+                      ),
+                      RaisedButton(
+                        onPressed: () {
+                          onSaved(context);
+                          // Navigator.of(context)
+                          //     .pushNamed(HomeScreen.routeName);
+                        },
+                        child: Text('Login'),
+                        textColor: Colors.purple,
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
