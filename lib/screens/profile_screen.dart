4353657ee0252100d741468fa619c9139e6b0c1d
unknown <19104010@mail.jiit.ac.in>
2020-10-24 22:46:28 +0530

    profile screen

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MyProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 15, left: 10),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_ios),
+                    onPressed: null,
+                  ),
+                  Container(
+                    width: 125.0,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.add_a_photo_rounded),
+                          onPressed: null,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: null,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
