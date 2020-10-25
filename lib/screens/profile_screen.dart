5a6bfe7b9f21c154d2bf7116571cba585a065d73
Amit Patil <amitgpatil215@gmail.com>
2020-10-25 12:48:32 +0530

    Improvements in profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -23,7 +23,7 @@ class ProfileScreenState extends State<ProfileScreen> {
         fit: BoxFit.cover,
       );
     } else {
-      return Image.asset(
+      return SvgPicture.asset(
         'assets/images/blank_canvas.svg',
         width: 200,
         height: 200,
@@ -43,142 +43,144 @@ class ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.orange[100],
-        body: ListView(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 15, left: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back_ios),
-                    onPressed: null,
-                    color: Colors.white,
-                  ),
-                  Container(
-                    width: 125.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.add_a_photo_rounded),
-                          onPressed: getImage(ImageSource.camera),
-                          //add the image picker here after it select the hashtag
-                          color: Colors.white,
-                        ),
-                        IconButton(
-                            icon: Icon(Icons.menu),
-                            onPressed: null,
-                            color: Colors.white)
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Stack(
+    return Scaffold(
+      backgroundColor: Colors.orange[100],
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 15, left: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.all(30),
-                      child: Text(
-                        'My Profile',
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
-                          color: Colors.orange[700],
-                          fontWeight: FontWeight.bold,
-                          fontSize: 35,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: double.maxFinite,
-                      alignment: Alignment.centerRight,
-                      child: Card(
-                        //if user could insert a profile picture
-                        //child: PickImage(),
-                        //under written code if for reference where
-                        //our profile pic should appear
-                        child: getImageWidget(),
-                      ),
-                    ),
-                  ],
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  color: Colors.white,
                 ),
-                Column(
-                  children: [
-                    Opacity(
-                      opacity: 0.5,
-                      child: Container(
-                        child: SvgPicture.asset(
-                          'assets/images/choice.svg',
-                          height: 300.0,
-                          width: 420.0,
-                          allowDrawingOutsideViewBox: true,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 500,
-                      width: double.maxFinite,
-                      decoration: BoxDecoration(
+                Container(
+                  width: 125.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.add_a_photo_rounded),
+                        onPressed: () {
+                          getImage(ImageSource.camera);
+                        },
+                        //add the image picker here after it select the hashtag
                         color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(100),
-                        ),
                       ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          //add bio entered by the user
-                          Card(
-                            child: Text(
-                              'Bio taken from user in the center',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.deepOrange[300],
-                              ),
-                            ),
-                          ),
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                FlatButton(
-                                  onPressed: null,
-                                  child: Text(
-                                    'ArtWork',
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                    ),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ),
-                                FlatButton(
-                                  onPressed: null,
-                                  child: Text(
-                                    'Edit Profile',
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                    ),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ),
-                              ]),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                      IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: null,
+                          color: Colors.white)
+                    ],
+                  ),
+                )
               ],
             ),
-          ],
-        ),
+          ),
+          Stack(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(30),
+                    child: Text(
+                      'My Profile',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: Colors.orange[700],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    alignment: Alignment.centerRight,
+                    child: Card(
+                      //if user could insert a profile picture
+                      //child: PickImage(),
+                      //under written code if for reference where
+                      //our profile pic should appear
+                      child: getImageWidget(),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Opacity(
+                    opacity: 0.5,
+                    child: Container(
+                      child: SvgPicture.asset(
+                        'assets/images/choice.svg',
+                        height: 300.0,
+                        width: 420.0,
+                        allowDrawingOutsideViewBox: true,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 500,
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(100),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        //add bio entered by the user
+                        Card(
+                          child: Text(
+                            'Bio taken from user in the center',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.deepOrange[300],
+                            ),
+                          ),
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              FlatButton(
+                                onPressed: null,
+                                child: Text(
+                                  'ArtWork',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              FlatButton(
+                                onPressed: null,
+                                child: Text(
+                                  'Edit Profile',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ]),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
